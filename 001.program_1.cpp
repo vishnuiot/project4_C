438203d0a90f3ada168d156bdd73ec60909175f0
vishnuiot <vishnuprasad.iit@gmail.com>
2025-07-08 13:04:14 +0200

    test

--- 001.program_1.cpp
+++ 001.program_1.cpp
@@ -6,5 +6,19 @@ int main()
     std::cout << "Hallo welt" << std::endl;
     return 0;  
 }
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int anzahl;
+    double preis;
+
+    anzahl=2;
+    preis=1.4;
+    cout << "anzahl= " << anzahl << endl;
+    cout << "preis= " << preis << endl;
+    return 0;
+}
  
 
