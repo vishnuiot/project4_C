58e7cc799e1122d536d43375a3f4bfa05f326e09
vishnuiot <vishnuprasad.iit@gmail.com>
2025-07-10 22:56:29 +0200

    test

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+    // int anzahlApfel=2, anzahlBirne=3;
+    // double preisApfel=4.55,preisBirne=6.77;
+    // double summe,mittel,anteil,differenz;
+    // summe=anzahlApfel*preisApfel+anzahlBirne*preisBirne;
+    // mittel=summe/(anzahlApfel+anzahlBirne);
+    // anteil=100*anzahlApfel/(anzahlApfel+anzahlBirne);
+    // differenz=preisApfel-preisBirne;
+    // cout << "summe= " << summe << endl;
+    // cout << "mittel= " << mittel << endl;
+    // cout << "anteil= " << anteil << endl;
+    // cout << "differenz= " << differenz << endl;
