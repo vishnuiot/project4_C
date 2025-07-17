1d366bf2e8661543b3ca7f75b461914284dd6967
vishnuiot <vishnuprasad.iit@gmail.com>
2025-07-17 10:33:41 +0200

    git commit history created

---
+++ 100.github_instructions.sh
@@ -0,0 +1,14 @@
+# rm -fr .git  
+# git init
+# git branch -m Grundkurs_C  # rename local brach to remote or any other name_ like master
+# git add .
+# git commit -m "Git commit history created"
+# git remote add origin https://github.com/vishnuiot/project4_Embedded_C.git
+# git config pull.rebase false # tells git to use a merge strategy by default
+# git pull https://github.com/vishnuiot/project4_Embedded_C/
+# git commit -m "Git rebase"
+# git pull origin main --allow-unrelated-histories
+# git push origin main
+# git log --graph --oneline --decorate --all  ## on terminal displays the commit history
+# git config --global push.autoSetupRemote true
+# git push
