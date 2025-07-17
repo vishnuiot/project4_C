6733f224656a788e02ac288f9d3b7894df7f70ec
vishnuiot <vishnuprasad.iit@gmail.com>
2025-07-17 09:32:55 +0200

    git commit -m "EC-17 <message>"

---
+++ 002.program_2.10_zahlensystem.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=20,b=20;
+    cout <<"oktalzahl ="<<oct<<a<<endl;
+    cout <<"hexadezimalzahl = "<<hex<<a<<endl;
+    cout <<"dezimalzahl = "<<dec<<a<<endl;
+
+}
