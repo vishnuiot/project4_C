31f43e91e8c9d23cf17aa8f513843d09c7f9aa76
vishnuiot <vishnuprasad.iit@gmail.com>
2025-07-18 10:46:00 +0200

    git commit -m "WW007-7 <message>"

--- 2.ErsteSchritte/bsp10001.c
+++ 2.ErsteSchritte/bsp10001.c
@@ -5,5 +5,8 @@
 int main(int argc, char const *argv[])
 {
     /* code */
+    printf("Dies ist ein C Program \n");
+    printf("nicht nur Texte Drucken \n");
+    printf("sondern auch rechnen können \n");
     return 0;
 }
